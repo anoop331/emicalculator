a1f174916bc89bd5f222fcf20bc684bd397fcecb
Anoop <anoop.k.mathew@live.com>
2017-09-09 13:10:48 +0200

    Adding the source to git

---
+++ BankInterestCalculator/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace BankInterestCalculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultLabel = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.closeAfter = new System.Windows.Forms.TextBox();
+            this.remainingLabel = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(142, 51);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "3000000";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(142, 102);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "9.75";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Principle";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 105);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Annual percentage";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(95, 279);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultLabel
+            // 
+            this.resultLabel.AutoSize = true;
+            this.resultLabel.Location = new System.Drawing.Point(22, 221);
+            this.resultLabel.Name = "resultLabel";
+            this.resultLabel.Size = new System.Drawing.Size(29, 13);
+            this.resultLabel.TabIndex = 5;
+            this.resultLabel.Text = "EMI:";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(142, 147);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "240";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Number of Months";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 185);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(89, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Close after month";
+            // 
+            // closeAfter
+            // 
+            this.closeAfter.Location = new System.Drawing.Point(142, 178);
+            this.closeAfter.Name = "closeAfter";
+            this.closeAfter.Size = new System.Drawing.Size(100, 20);
+            this.closeAfter.TabIndex = 9;
+            this.closeAfter.Text = "60";
+            // 
+            // remainingLabel
+            // 
+            this.remainingLabel.AutoSize = true;
+            this.remainingLabel.Location = new System.Drawing.Point(22, 243);
+            this.remainingLabel.Name = "remainingLabel";
+            this.remainingLabel.Size = new System.Drawing.Size(97, 13);
+            this.remainingLabel.TabIndex = 10;
+            this.remainingLabel.Text = "Remaining to close";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 340);
+            this.Controls.Add(this.remainingLabel);
+            this.Controls.Add(this.closeAfter);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.resultLabel);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label resultLabel;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox closeAfter;
+        private System.Windows.Forms.Label remainingLabel;
+    }
+}
+
---
+++ BankInterestCalculator/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BankInterestCalculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var p = double.Parse(textBox1.Text);
+            var r = double.Parse(textBox2.Text)/12;
+            var n = int.Parse(textBox3.Text);
+            var c = int.Parse(closeAfter.Text);
+            var result = Calculate(p,r,n);
+            var total = result*n;
+            var left = total - c*result;
+            resultLabel.Text = result.ToString();
+            remainingLabel.Text = CalculateRemaining(p, result, c, r).ToString();
+        }
+
+        private double CalculateRemaining(double p, double e, int n, double r)
+        {
+            var x = 1 + r / 100;
+            return p*Math.Pow(x, n) - ((SumSeries(x,n) - 1))*e;
+        }
+
+        private double Calculate(double p, double r, int n)
+        {
+            var x = 1 + r/100;
+            return p*Math.Pow(x, n)/SumSeries(x, n);
+        }
+
+        private double SumSeries(double x, int n)
+        {
+            if (n == 1) return 1;
+            return SumSeries(x, n - 1) + Math.Pow(x, n - 1);
+        }
+
+    }
+}
